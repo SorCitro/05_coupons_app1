3894fe7a0f8dfb3f7286aca6d815e2fda694300b
SorCitro <ingrid.marti@hotmail.com>
2023-12-29 10:01:53 +0100

    back botton working

--- src/app/pages/coupons/coupons.page.ts
+++ src/app/pages/coupons/coupons.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from '@ionic/angular';
+import { Coupon } from 'src/app/models/coupons';
+import { CouponsService } from 'src/app/services/coupons.service';
 
 @Component({
   selector: 'app-coupons',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CouponsPage implements OnInit {
 
-  constructor() { }
+  public coupons: Coupon[];
+
+  constructor(
+    private couponsServices: CouponsService,
+    private navParams: NavParams,
+    private navController: NavController
+
+
+  ) { 
+    this.coupons = [];
+  }
 
   ngOnInit() {
+    this.couponsServices.getCoupons().then((coupons: Coupon[])=>{
+      this.coupons = coupons;
+      console.log(this.coupons);
+    })
+  }
+
+  changeActive(coupon: Coupon){
+    coupon.active = !coupon.active;
+  }
+
+  goToCard(){
+    this.navParams.data["coupons"]= this.coupons.filter
+    (c => c.active);
+    this.navController.navigateForward('card-coupon');
   }
 
 }
